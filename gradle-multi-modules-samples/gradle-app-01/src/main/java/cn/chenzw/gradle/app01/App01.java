aa400aac5a8f285f52b288cd66289b09b4bbe886
chenzw <656469722@qq.com>
2019-08-08 23:44:44 +0800

    initialization

---
+++ gradle-multi-modules-samples/gradle-app-01/src/main/java/cn/chenzw/gradle/app01/App01.java
@@ -0,0 +1,8 @@
+package cn.chenzw.gradle.app01;
+
+public class App01 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ gradle-multi-modules-samples/gradle-app-core/src/main/java/cn/chenzw/gradle/app/core/constants/Constants.java
@@ -0,0 +1,6 @@
+package cn.chenzw.gradle.app.core.constants;
+
+public class Constants {
+
+    public static final String APP_NAME = "gradle-app-core";
+}
